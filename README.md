0b468faf81b80de165287d3df707e467c794cc25
muhammad-abdulqayyum <muhammadaq93@yahoo.com>
2016-11-14 12:02:01 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Mobile-Office-Application
+A simple and effective mobile office application that aims to combine notepad, calculator, calendar and other features into one android application
---
+++ project/app/src/main/java/com/example/hafiz/officeapp/MainActivity.java
@@ -0,0 +1,72 @@
+package com.example.hafiz.officeapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    void startNotepad (View view)
+    {
+        Intent intent = new Intent(this, NotepadActivity.class);
+        startActivity(intent);
+    }
+
+    void startDrawing (View view)
+    {
+        Intent intent = new Intent(this, DrawingActivity.class);
+        startActivity(intent);
+    }
+
+    void startCalculator (View view)
+    {
+        Intent intent = new Intent(this, CalculatorActivity.class);
+        startActivity(intent);
+    }
+
+    void startScheduling (View view)
+    {
+        Intent intent = new Intent(this, SchedulingActivity.class);
+        startActivity(intent);
+    }
+
+
+
+}
